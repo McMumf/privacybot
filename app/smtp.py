63ca001f59d89a4c7683117f0d1b4b23c1f92d94
Carson <mccaffreycarson@gmail.com>
2024-09-24 22:11:34 -0400

    Start actually wiring up SMTP for testing

--- app/smtp.py
+++ app/smtp.py
@@ -1,18 +1,29 @@
 import smtplib, ssl
+from os import getenv
 
 smtp_server = "smtp.gmail.com"
 port = 587  # For starttls
-sender_email = "my@gmail.com"
-password = input("Type your password and press enter: ")
+
 
 class smtp_client:
     """
     Configures SMTP client for secure
     """
-    def __init__(self, api_token: str):
-        context = ssl.create_default_context()
+    def __init__(self):
+        self.sender_email = getenv('SENDER_EMAIL_ADDR')
+        self.password = getenv('SMTP_API_KEY')
+
+        self.context = ssl.create_default_context()
+
         self.server = smtplib.SMTP(smtp_server, port)
-        self.server.starttls(context=context) # Secure the connection
+        self.server.starttls(context=self.context) # Secure the connection
+        self.server.login( self.sender_email,  self.password)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.server.quit()
 
     def send_email(self, sender: str, receiver: str, contents: str):
-        self.server.sendmail(sender, receiver, contents)
+        try:
+            self.server.sendmail(sender, receiver, contents)
+        except Exception as e:
+            print(e)
